30e554f7f44c370a4ceb65b09b41824d4456a3a7
AbdelrahmanShaheen <77184432+AbdelrahmanShaheen@users.noreply.github.com>
2022-09-26 05:25:23 +0200

    Time: 81 ms (93.78%), Space: 27.8 MB (64.08%) - LeetHub

---
+++ 436-find-right-interval/436-find-right-interval.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> findRightInterval(vector<vector<int>>& intervals) {
+        
+        map<int ,int>start_to_idx; // start,index of interval before sorting.
+        
+        for(int i= 0 ;i < intervals.size() ;i++){
+            start_to_idx[intervals[i][0]] = i;
+        }
+        vector<int>ans(intervals.size() ,-1); //sorting.
+        
+        //Doing the binary search.
+        for(int i= 0 ;i < intervals.size() ;i++){
+            auto it = start_to_idx.lower_bound(intervals[i][1]);
+            if(it != start_to_idx.end())
+            ans[i] =  it->second;
+        }
+        return ans;
+    }
+    //nlong(n) --> time complexity.
+};

000b9121530288b785979bbfbfd733611788dc32
AbdelrahmanShaheen <77184432+AbdelrahmanShaheen@users.noreply.github.com>
2022-09-27 08:55:05 +0200

    Time: 695 ms (20.65%), Space: 13 MB (5.80%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.cpp
+++ 611-valid-triangle-number/611-valid-triangle-number.cpp
@@ -1,22 +1,5 @@
 class Solution {
 public:
-    
-    int binary_search(vector<int>& nums, int start ,int val){
-        int end = nums.size() - 1;
-        int pos = 0;
-        while(start <= end){
-            int mid = start + (end - start) / 2;
-            if(val > nums[mid])
-            {
-                start = mid + 1;
-                pos = mid;
-            }
-            else
-                end = mid - 1;
-        }
-        return pos;
-    }
-    
     int triangleNumber(vector<int>& nums) {
         int sz = nums.size();
         int cnt = 0;
@@ -24,9 +7,13 @@ public:
         for(int i = 0 ;i < sz-2; i++){
             
             for(int j = i+1 ;j < sz - 1; j++){
-                int idx = binary_search(nums ,j+1 ,nums[i] + nums[j]);
-                if(idx)
-                cnt += idx - j;
+                auto it = lower_bound(nums.begin()+(j+1) ,nums.end() ,nums[i] + nums[j]); 
+                int idx = sz;
+                if(it != nums.end())
+                {
+                    idx = it - nums.begin();
+                }
+                cnt += idx - j - 1;
             }
         }
         return cnt;
